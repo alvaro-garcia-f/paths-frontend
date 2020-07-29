1b16a2bb23d244b9d810c07114ff431c275adaba
Álvaro García <alvaro.garcia.f@gmail.com>
2020-07-29 16:15:20 +0100

    Main view and Lesson view modified. Lesson content split in title and body text using markers

--- src/views/Lesson.vue
+++ src/views/Lesson.vue
@@ -3,8 +3,11 @@
     <v-carousel hide-delimiters :continuous=false>
       <v-carousel-item v-for="(slide, idx) in content" :key="idx">
         <v-card height="100%">
+          <v-card-title>
+            <h4>{{ getTitle (slide) }}</h4>
+          </v-card-title>
           <v-card-text>
-            <p>{{ slide }}</p>
+            <p>{{ getContent (slide) }}</p>
           </v-card-text>
         </v-card>
       </v-carousel-item>
@@ -17,6 +20,15 @@ export default {
   name: 'Lesson',
   props: {
     content: Array
+  },
+  methods: {
+    getTitle (slide) {
+      return slide.split('-->')[0]
+    },
+
+    getContent (slide) {
+      return slide.split('-->')[1]
+    }
   }
 }
 </script>
