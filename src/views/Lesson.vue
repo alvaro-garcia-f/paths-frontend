01d1acb3e81a963e0c82f85b79e4d44fcb948a55
Álvaro García <alvaro.garcia.f@gmail.com>
2020-08-06 23:05:10 +0100

    Lesson.vue - Changed presentation size

--- src/views/Lesson.vue
+++ src/views/Lesson.vue
@@ -15,25 +15,12 @@
       <v-divider></v-divider>
     </v-row>
 
-    <v-row class="mt-2" v-if="lesson.url">
-      <v-col class="google-slides-container">
-        <p v-html="lesson.url"></p>
+    <v-row class="mt-2 align-center justify-center google-slides-container" v-if="lesson.url" align="center">
+      <v-col class="text-center">
+        <div v-html="lesson.url"></div>
       </v-col>
     </v-row>
 
-    <v-carousel class="mt-2" hide-delimiters :continuous=false v-else>
-      <v-carousel-item v-for="(slide, idx) in lesson.content" :key="idx">
-        <v-card height="100%">
-          <v-card-title>
-            <h4>{{ getTitle (slide) }}</h4>
-          </v-card-title>
-
-          <v-card-text>
-            <p>{{ getContent (slide) }}</p>
-          </v-card-text>
-        </v-card>
-      </v-carousel-item>
-    </v-carousel>
   </v-container>
 </template>
 
@@ -69,18 +56,13 @@ export default {
 </script>
 
 <style>
-.google-slides-container{
-    position: relative;
-    width: 100%;
-    padding-top: 60%;
-    overflow: hidden;
+
+.google-slides-container {
+  height: 70vh;
 }
 
 .google-slides-container iframe{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+    width: 80vw;
+    height: 70vh;
 }
 </style>
